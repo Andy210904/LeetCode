b58ca27bc38ee762cbdc3c7c9fcc76b091487f6c
Andy210904 <128392513+Andy210904@users.noreply.github.com>
2025-01-04 11:11:58 +0530

    Time: 136 ms (88.65%), Space: 15.8 MB (69.02%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        vector<int> alpha(26,0);
-        for(int i=0;i<s.size();i++){
-            alpha[s[i] - 'a']++;
+        vector<int> alpha(26, 0);
+        for (char c : s) {
+            alpha[c - 'a']++;
         }
-        int count = 0;
-        for(int i=0;i<26;i++){
-            int left = 0;
-            int right = s.size()-1;
-            unordered_set<char> temp;
-            if(alpha[i] > 1){
-                while(s[left] != (i+'a')){
+        int count = 0; 
+        for (int i = 0; i < 26; i++) {
+            if (alpha[i] > 1) { 
+                int left = 0, right = s.size() - 1;
+                unordered_set<char> temp; 
+                while (s[left] != (i + 'a')) {
                     left++;
                 }
-                while(s[right] != (i+'a')){
+                while (s[right] != (i + 'a')) {
                     right--;
                 }
-                for(int j=left+1;j<right;j++){
-                    if(temp.find(s[j]) == temp.end()){
+                for (int j = left + 1; j < right; j++) {
+                    if (temp.find(s[j]) == temp.end()) { 
                         temp.insert(s[j]);
                         count++;
-                    }
-                    else{
-                        continue;
                     }
                 }
             }
