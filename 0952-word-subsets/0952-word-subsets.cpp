5b03fd03571151c5bc311a3751a5e14dda2c4dc0
Andy210904 <128392513+Andy210904@users.noreply.github.com>
2025-01-10 10:45:44 +0530

    Time: 61 ms (53.88%), Space: 106.7 MB (49.77%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.cpp
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
         vector<int> req(26, 0);
-        for (const string& word : words2) {
-            vector<int> freq(26, 0);
-            for (char c : word) {
-                freq[c - 'a']++;
+        for (int i = 0; i < words2.size(); i++) {
+            vector<int> temp(26, 0);
+            for (int j = 0; j < words2[i].size(); j++) {
+                temp[words2[i][j] - 'a']++;
             }
-            for (int i = 0; i < 26; i++) {
-                req[i] = max(req[i], freq[i]);
+            for (int k = 0; k < 26; k++) {
+                req[k] = max(req[k], temp[k]);
             }
         }
         vector<string> res;
-        for (const string& word : words1) {
+        for (int i = 0; i < words1.size(); i++) {
             vector<int> temp(26, 0);
-            for (char c : word) {
-                temp[c - 'a']++;
+            for (int j = 0; j < words1[i].size(); j++) {
+                temp[words1[i][j] - 'a']++;
             }
             bool isSubset = true;
-            for (int i = 0; i < 26; i++) {
-                if (temp[i] < req[i]) {
+            for (int k = 0; k < 26; k++) {
+                if (req[k] > 0 && temp[k] < req[k]) {
                     isSubset = false;
                     break;
                 }
             }
             if (isSubset) {
-                res.push_back(word);
+                res.push_back(words1[i]);
             }
         }
         return res;
