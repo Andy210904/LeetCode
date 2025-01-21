fac9e87abe9e58d51b7d135d5933bf5333d979ea
Andy210904 <128392513+Andy210904@users.noreply.github.com>
2025-01-21 15:04:00 +0530

    Time: 0 ms (100%), Space: 64.3 MB (43.19%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
-    string to_bi(int number) {
-        return bitset<32>(number).to_string();
-    }
-    int biToInt(const string& binaryString) {
-        return bitset<32>(binaryString).to_ulong(); 
-    }
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
-        vector<long> req(32,0);
-        int n = nums1.size();
-        int m = nums2.size();
-        for(int i=0;i<n;i++){
-            string temp = to_bi(nums1[i]);
-            for(int i=0;i<32;i++){
-                if(temp[i] == '1'){
-                    req[i] = req[i]+m;
-                }
+        int count1 = nums1.size();
+        int count2 = nums2.size();
+        int result = 0;
+        if(count1 %2){
+            for(int num : nums2){
+                result = result ^ num;
             }
         }
-        for(int i=0;i<m;i++){
-            string temp = to_bi(nums2[i]);
-            for(int i=0;i<32;i++){
-                if(temp[i] == '1'){
-                    req[i] = req[i]+n;
-                }
+        if(count2 %2){
+            for(int num : nums1){
+                result = result ^ num;
             }
         }
-        string binaryString(32, '0');
-        for(int i=0;i<32;i++){
-            if(req[i]!=0 && req[i]%2 == 1){
-                binaryString[i] = '1';
-            }
-        }
-        int result = biToInt(binaryString);
         return result;
+        
     }
 };
