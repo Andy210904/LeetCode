9b475e7fc0e7a344c91bc1c9fd719f9b31a0e39d
Andy210904 <128392513+Andy210904@users.noreply.github.com>
2025-04-17 02:12:19 +0530

    Time: 80 ms (61.7%), Space: 79 MB (77.07%) - LeetHub

--- 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.cpp
+++ 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.cpp
@@ -5,13 +5,9 @@ public:
         long long cnt = 0;
         unordered_map<int, int> freq;
         long long pairs = 0;
-
         for (int right = 0; right < nums.size(); ++right) {
-            // Add current element to the frequency map
             pairs += freq[nums[right]];
             freq[nums[right]]++;
-
-            // Shrink the window until pairs < k
             while (pairs >= k) {
                 cnt += nums.size() - right;
                 freq[nums[left]]--;
