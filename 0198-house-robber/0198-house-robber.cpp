661ca7a2224b7529e1f004ea071050f46e04190d
Aditya Dhane <128392513+Andy210904@users.noreply.github.com>
2025-08-01 19:11:53 +0530

    Time: 0 ms (100%), Space: 10.6 MB (64.28%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+    //memoiazation
+    // int f(vector<int>& arr,int n,vector<int>&dp){
+    //     if(n == 0){
+    //         return dp[n] = arr[n];
+    //     }
+    //     if(n < 0)return 0;
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
+    //     int pick = arr[n] + f(arr,n-2,dp);
+    //     int notpick =f(arr,n-1,dp);
+    //     return dp[n] = max(pick,notpick);
+    // }
     int rob(vector<int>& arr) {
         int n = arr.size();
         vector<int> dp(n+1,-1);
