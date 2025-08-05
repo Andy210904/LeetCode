89e07022f06a5da2c8680494d5f1b6afe3468b15
Aditya Dhane <128392513+Andy210904@users.noreply.github.com>
2025-08-05 10:01:58 +0530

    Time: 7 ms (69.19%), Space: 15.4 MB (47.98%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
-    int backtrack(vector<int>& nums,int ind,int target){
+    int subset(int ind, int sum, vector<int>& arr, vector<vector<int>>& dp) {
         if(ind == 0){
-            return (nums[0] == target) + (nums[0] == -target);
+            if(sum == 0 && arr[0] == 0) return 2;
+            if(sum == 0 || arr[0] == sum) return 1;
+            return 0;
         }
-        int add = backtrack(nums,ind-1,target-nums[ind]);
-        int sub = backtrack(nums,ind-1,target+nums[ind]);
-        return add+sub;
+        if(dp[ind][sum] != -1) return dp[ind][sum];
+
+        int notTake = subset(ind-1, sum, arr, dp);
+        int take = 0;
+        if(arr[ind] <= sum)
+            take = subset(ind-1, sum-arr[ind], arr, dp);
+
+        return dp[ind][sum] = (take + notTake);
+    }
+    int perfectSum(vector<int>& arr, int sum) {
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(sum+1, -1));
+        return subset(n-1, sum, arr, dp);
+    }
+    int countPartitions(vector<int>& arr, int d) {
+        // Code here
+        int total = 0;
+        for(int i=0;i<arr.size();i++){
+            total += arr[i];
+        }
+        if(total-d < 0 || (total-d)% 2 == 1) return false;
+        return perfectSum(arr,(total-d)/2);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        return backtrack(nums,n-1,target);
+        return countPartitions(nums,target);
     }
 };
