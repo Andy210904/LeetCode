d774b0ee9fba341c85e2f183c76a5c37dd415d60
Aditya Dhane <128392513+Andy210904@users.noreply.github.com>
2025-08-14 14:48:59 +0530

    Time: 3 ms (26.03%), Space: 23 MB (43.37%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> left(n, 1);
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            }
+        }
+        int sum = left[n - 1];
+        int right = 1;         
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                right++;
+            } else {
+                right = 1;
+            }
+            sum += max(left[i], right);
+        }
+        return sum;
+    }
+};
