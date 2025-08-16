e361ec9ba57c71244ae2a8b662c07e654f637990
Aditya Dhane <128392513+Andy210904@users.noreply.github.com>
2025-08-16 22:01:40 +0530

    Time: 0 ms (100%), Space: 8 MB (71.54%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string num1 = to_string(num);
+        int n = num1.size();
+        for(int i=0;i<n;i++){
+            if(num1[i] == '6'){
+                num1[i] = '9';
+                break;
+            }
+        }
+        return stoi(num1);
+    }
+};
