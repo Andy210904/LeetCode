157b7ef75b784e3d7e063822586a1a1121b2ea63
Aditya Dhane <128392513+Andy210904@users.noreply.github.com>
2025-08-31 12:48:03 +0530

    Time: 32 ms (49.99%), Space: 54.4 MB (35.84%) - LeetHub

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int numSubseq(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        int mod = 1000000007;
+        const int mod = 1000000007;
         int n = nums.size();
-        vector<int> pow2(n, 1);
-        for (int i = 1; i < n; ++i) {
-            pow2[i] = (pow2[i - 1] * 2) % mod;
+        sort(nums.begin(), nums.end());
+
+        // precompute powers of 2 modulo mod otherwise it will be slow and out of bound issues
+        vector<int> power(n, 1);
+        for (int i = 1; i < n; i++) {
+            power[i] = (power[i - 1] * 2LL) % mod;
         }
-        int count = 0;
         int left = 0, right = n - 1;
+        int res = 0;
         while (left <= right) {
             if (nums[left] + nums[right] <= target) {
-                count = (count + pow2[right - left]) % mod;
+                res = (res + power[right - left]) % mod;
                 left++;
             } else {
                 right--;
             }
         }
-        return count;
+        return res;
     }
 };
