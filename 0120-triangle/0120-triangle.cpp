2a58a8da6129d491fd08f4daeb729ec4f1e157cd
Aditya Dhane <128392513+Andy210904@users.noreply.github.com>
2025-09-25 14:52:49 +0530

    Time: 0 ms (100%), Space: 13.3 MB (42.12%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    // int minsum(vector<vector<int>>& triangle,int i,int j,int n,vector<vector<int>>& dp){
-    //     if(i == n-1){
-    //         return triangle[i][j];
-    //     }
-    //     if(dp[i][j] != -1){
-    //         return dp[i][j];
-    //     }
-    //     int same = triangle[i][j] + minsum(triangle,i+1,j,n,dp);
-    //     int diff = triangle[i][j] + minsum(triangle,i+1,j+1,n,dp);
-    //     return dp[i][j] = min(same,diff);
-    // }
+    int minsum(vector<vector<int>>& triangle,int i,int j,int n,vector<vector<int>>& dp){
+        if(i == n-1){
+            return triangle[i][j];
+        }
+        if(dp[i][j] != INT_MAX){
+            return dp[i][j];
+        }
+        int same = triangle[i][j] + minsum(triangle,i+1,j,n,dp);
+        int diff = triangle[i][j] + minsum(triangle,i+1,j+1,n,dp);
+        return dp[i][j] = min(same,diff);
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        for(int i=0;i<n;i++){
-            dp[n-1][i] = triangle[n-1][i];
-        }
-        for(int i=n-2;i>=0;i--){
-            for(int j=i;j>=0;j--){
-                int same = triangle[i][j] + dp[i+1][j];
-                int diff = triangle[i][j] + dp[i+1][j+1];
-                dp[i][j] = min(same,diff);
-            }
-        }
-        return dp[0][0];
+        vector<vector<int>> dp(n,vector<int>(n,INT_MAX));
+        return minsum(triangle,0,0,n,dp);
+        // for(int i=0;i<n;i++){
+        //     dp[n-1][i] = triangle[n-1][i];
+        // }
+        // for(int i=n-2;i>=0;i--){
+        //     for(int j=i;j>=0;j--){
+        //         int same = triangle[i][j] + dp[i+1][j];
+        //         int diff = triangle[i][j] + dp[i+1][j+1];
+        //         dp[i][j] = min(same,diff);
+        //     }
+        // }
+        // return dp[0][0];
     }
 };
