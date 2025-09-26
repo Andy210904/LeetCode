8f1a85c20f47560e3298817c7fd378a159487bf5
Aditya Dhane <128392513+Andy210904@users.noreply.github.com>
2025-09-26 18:18:34 +0530

    Time: 1802 ms (36.58%), Space: 9.2 MB (48.17%) - LeetHub

---
+++ 1429-verbal-arithmetic-puzzle/1429-verbal-arithmetic-puzzle.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    long long getnum(string &s, vector<int> &charmp){
+        long long num = 0;
+        for(auto ch:s){
+            int n = charmp[ch - 'A'];
+            num = num*10 + n;
+        }
+        return num;
+    }
+    bool solve(int ind, vector<char> &unique, vector<int> &charmp, vector<bool> &usednum, vector<string>& words, string &result, set<char> &firstnum){
+        if(ind >= unique.size()){
+            long long res = getnum(result, charmp), sum = 0;
+            for(auto &ch:words){
+                long long num = getnum(ch, charmp);
+                sum += num;
+                if(sum > res)return false; 
+            }
+            if(sum == res)return true;  
+            return false;
+        }
+        char ch = unique[ind]; 
+        for(int digit=0; digit<=9; digit++){
+            if(usednum[digit] == false){ 
+                if(digit==0 and firstnum.find(ch) != firstnum.end())continue;  
+                usednum[digit] = true;
+                charmp[ch - 'A'] = digit;
+                if(solve(ind+1, unique, charmp, usednum, words, result, firstnum) == true)return true;
+                charmp[ch - 'A'] = -1;
+                usednum[digit] = false;
+            }
+        }
+        return false;
+    }
+    bool isSolvable(vector<string>& words, string result) {
+        vector<int> charmp(26, -2); 
+        vector<bool> usednum(10, false); 
+        set<char> firstnum; 
+        vector<char> unique; 
+        for(auto word:words){
+            if(word.size() > result.size()) return false; 
+            if(word.size() > 1)firstnum.insert(word[0]); 
+            for(auto &ch:word){
+                if(charmp[ch-'A'] != -1){  
+                    unique.push_back(ch);
+                    charmp[ch-'A'] = -1;
+                }
+            }
+        }
+        if(result.size() > 1)firstnum.insert(result[0]);
+        for(auto ch:result){
+            if(charmp[ch-'A'] != -1){
+            unique.push_back(ch);
+            charmp[ch-'A'] = -1;
+            }
+        }
+        return solve(0, unique, charmp, usednum, words, result, firstnum);
+    }
+};
